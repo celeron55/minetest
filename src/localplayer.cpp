d4a520e268d3393a0ece36ae26a39d758feb1094
Perttu Ahola <celeron55@gmail.com>
2015-01-05 23:17:44 +0200

    Increase player_stepheight by 0.5 for not having to jump

--- src/localplayer.cpp
+++ src/localplayer.cpp
@@ -206,6 +206,7 @@ void LocalPlayer::move(f32 dtime, Environment *env, f32 pos_max_d,
 	}
 
 	float player_stepheight = touching_ground ? (BS*0.6) : (BS*0.2);
+	player_stepheight += BS*0.5;  // Glide uphills with magic
 
 	v3f accel_f = v3f(0,0,0);
 
